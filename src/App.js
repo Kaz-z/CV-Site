a0499eb5bb48e3d5f45d6e0b99c0cb03436d81c2
Kaazim Zia <sw004691@live.reading.ac.uk>
2019-07-28 20:32:53 +0100

    Updating url for resume data

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'/resumeData.json',
+      url:'https://kaz-z.github.io/CV-Site/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
